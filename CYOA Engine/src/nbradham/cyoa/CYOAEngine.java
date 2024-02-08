6eabd380c97c25b47a687435c6b8af23de3cfe02
nickolasbradham <nickolasbradham@gmail.com>
2024-02-08 10:27:09 -0700

    Code docs.

--- CYOA Engine/src/nbradham/cyoa/CYOAEngine.java
+++ CYOA Engine/src/nbradham/cyoa/CYOAEngine.java
@@ -35,7 +35,6 @@ import javax.swing.filechooser.FileNameExtensionFilter;
  * Handles all program execution.
  *
  * @author Nickolas S. Bradham
- *
  */
 final class CYOAEngine {
 
@@ -95,6 +94,9 @@ final class CYOAEngine {
     private byte n;
     private boolean run, bool0;
 
+    /**
+     * Handles GUI creation.
+     */
     private void start() {
         SwingUtilities.invokeLater(() -> {
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -160,6 +162,11 @@ final class CYOAEngine {
         });
     }
 
+    /**
+     * Closes the current file and opens the new one.
+     *
+     * @throws IOException
+     */
     private void openFile() throws IOException {
         if (raf != null) {
             raf.close();
@@ -169,9 +176,17 @@ final class CYOAEngine {
         vars.clear();
     }
 
+    /**
+     * Configures and opens the JFileChooser. If the user selects a file, Calls
+     * {@code fh} on the selected file.
+     *
+     * @param filter The filter to apply to the chooser.
+     * @param title The title to set to the dialogue window.
+     * @param fh The {@link FileHandler} to use on selection.
+     * @return Returns true if the handler was NOT ran.
+     */
     private boolean openJFC(FileNameExtensionFilter filter, String title, FileHandler fh) {
-        jfc.setFileFilter(filter);
-        jfc.setDialogTitle(title);
+        prepJFC(filter, title);
         jfc.setSelectedFile(F_EMPTY);
         if (jfc.showOpenDialog(frame) == JFileChooser.APPROVE_OPTION) {
             fh.handle(jfc.getSelectedFile());
@@ -180,11 +195,23 @@ final class CYOAEngine {
         return true;
     }
 
+    /**
+     * Sets the filter and dialogue title of the {@link JFileChooser}
+     *
+     * @param filter The filter to use.
+     * @param title The title to set.
+     */
     private void prepJFC(FileNameExtensionFilter filter, String title) {
         jfc.setFileFilter(filter);
         jfc.setDialogTitle(title);
     }
 
+    /**
+     * Parses the story until the end of the file is hit or a wait command is
+     * triggered.
+     *
+     * @throws IOException
+     */
     private void parseStory() throws IOException {
         run = true;
         while (run && (str0 = nextTok()) != null) {
@@ -276,21 +303,44 @@ final class CYOAEngine {
         }
     }
 
+    /**
+     * Outputs the rest of the current story line to the {@link TextArea}.
+     *
+     * @throws IOException
+     */
     private void printRest() throws IOException {
         area.append(readLine());
         area.append("\n");
     }
 
+    /**
+     * Creates a story option {@link NButton} and adds it to the option panel.
+     *
+     * @param jump The label to jump to when this button is pressed.
+     * @param label The button text.
+     */
     private void createButton(String jump, String label) {
         optPane.add(new NButton(jump, label));
         optPane.revalidate();
         optPane.repaint();
     }
 
+    /**
+     * Parses the current story line into a {@link button}.
+     *
+     * @throws IOException
+     * @see #createButton(java.lang.String, java.lang.String)
+     */
     private void createButton() throws IOException {
         createButton(nextTok(), raf.readLine());
     }
 
+    /**
+     * Tests the next story token as an expression.
+     *
+     * @param r The {@link Runnable} to execute if the expression is true.
+     * @throws IOException
+     */
     private void testExp(Runnable r) throws IOException {
         str0 = nextTok();
         n = 0;
@@ -318,6 +368,14 @@ final class CYOAEngine {
         }
     }
 
+    /**
+     * Performs comparison {@code c} and executes {@link Runnable} {@code r} if
+     * it is true.
+     *
+     * @param c The comparison to test.
+     * @param r The Runnable to execute on true.
+     * @throws IOException
+     */
     private void doComp(VarComp c, Runnable r) throws IOException {
         if (c.doComp(vars.getOrDefault(str0, 0f), str1.equals("''") ? "" : getStr1Val())) {
             r.run();
@@ -326,6 +384,12 @@ final class CYOAEngine {
         }
     }
 
+    /**
+     * Tries to convert {@code str1} to a float or tries to retrieve it from
+     * {@code vars}.
+     *
+     * @return The float retrieved.
+     */
     private Object getStr1Val() {
         try {
             return Float.parseFloat(str1);
@@ -334,6 +398,13 @@ final class CYOAEngine {
         }
     }
 
+    /**
+     * Moves the story forward to {@code lab}. Will loop through story again if
+     * it doesn't find {@code lab} between current location and end.
+     *
+     * @param lab The label to jump to.
+     * @throws IOException
+     */
     private void jump(String lab) throws IOException {
         if (forward(lab)) {
             raf.seek(0);
@@ -341,25 +412,53 @@ final class CYOAEngine {
         }
     }
 
+    /**
+     * Scans forward in the story to label {@code lab} or the end of the file.
+     *
+     * @param lab The label to move to.
+     * @return Returns true if {@code lab} was NOT found by the end of the file.
+     * @throws IOException
+     */
     private boolean forward(String lab) throws IOException {
         while ((str0 = nextTok()) != null && !(">l".equals(str0) && lab.equals(nextTok())));
         return str0 == null;
     }
 
+    /**
+     * Performs {@code o} on {@code str0} in {@code vars} and {@code str1}.
+     * Stores the result back in {@code str0} in {@code vars}
+     *
+     * @param o The {@link  VarOp} to perform.
+     */
     private void op(VarOp o) {
         f = (float) getStr1Val();
         vars.put(str0, o.doOp((float) vars.getOrDefault(str0, 0f), f));
     }
 
+    /**
+     * Splits {@code str0} at {@code n} and puts the latter half in
+     * {@code str1}.
+     */
     private void splitStr() {
         str1 = str0.substring(n + 1);
         str0 = str0.substring(0, n);
     }
 
+    /**
+     * Finds the first non-letter, non-digit, character in {@code str0} and sets
+     * {@code n} to the index.
+     */
     private void toSym() {
         while (++n < str0.length() && Character.isLetterOrDigit(c = str0.charAt(n)));
     }
 
+    /**
+     * Parses the rest of the story line. Replaces {@code <VAR NAME>} with the
+     * variable value. Also respects the escape ({@code \}) character.
+     *
+     * @return Returns the complete String.
+     * @throws IOException
+     */
     private String readLine() throws IOException {
         sb0.setLength(0);
         while ((c = (char) raf.read()) != '\n') {
@@ -385,6 +484,12 @@ final class CYOAEngine {
         return sb0.toString();
     }
 
+    /**
+     * Retrieves the next, white space terminated, token from the story.
+     *
+     * @return Returns the completed token.
+     * @throws IOException
+     */
     private String nextTok() throws IOException {
         sb0.setLength(0);
         while ((c = (char) raf.read()) != 0xFFFF && !Character.isWhitespace(c)) {
@@ -393,6 +498,14 @@ final class CYOAEngine {
         return c == 0xFFFF && sb0.length() == 0 ? null : sb0.toString();
     }
 
+    /**
+     * Creates a new JMenuItem and sets up the menu.
+     *
+     * @param txt The label text.
+     * @param act The action listener on click.
+     * @param accel The accelerator key.
+     * @return Returns the new JMenuItem instance.
+     */
     private static JMenuItem createMenuItem(String txt, ActionListener act, char accel) {
         JMenuItem item = new JMenuItem(txt);
         item.addActionListener(act);
@@ -400,15 +513,29 @@ final class CYOAEngine {
         return item;
     }
 
+    /**
+     * Constructs and starts a new CYOAEngine instance.
+     *
+     * @param args Ignored.
+     */
     public static void main(String[] args) {
         System.out.println(System.getProperty("user.dir"));
         new CYOAEngine().start();
     }
 
+    /**
+     * A JButton that holds the jump label and {@link ActionListener}.
+     */
     private final class NButton extends JButton {
 
         private final String j;
 
+        /**
+         * Constructs a new NButton.
+         *
+         * @param jump The label to jump to.
+         * @param label The button text.
+         */
         private NButton(String jump, String label) {
             super(label);
             j = jump;
@@ -427,21 +554,49 @@ final class CYOAEngine {
         }
     }
 
+    /**
+     * Used to handle {@link File}s.
+     */
     @FunctionalInterface
     private static interface FileHandler {
 
+        /**
+         * Called when a File needs handled.
+         *
+         * @param fl The File to handle.
+         */
         void handle(File fl);
     }
 
+    /**
+     * Handles story variable operations.
+     */
     @FunctionalInterface
     private static interface VarOp {
 
+        /**
+         * Performs the operation.
+         *
+         * @param a The first variable.
+         * @param b The second variable.
+         * @return Returns the output of the operation.
+         */
         float doOp(float a, float b);
     }
 
+    /**
+     * Handles story variable comparisons.
+     */
     @FunctionalInterface
     private static interface VarComp {
 
+        /**
+         * Performs the comparison.
+         *
+         * @param a The first variable.
+         * @param b The second variable.
+         * @return Returns the comparison result.
+         */
         boolean doComp(Object a, Object b);
     }
 }
